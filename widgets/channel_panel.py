7a6aabef1c174a7c6c033aad1052d4f5d8b022bb
thomas147329 <thomasjhebda@gmail.com>
2026-08-07 09:37:17 -0500

    Add encoder channel control functions

--- widgets/channel_panel.py
+++ widgets/channel_panel.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 
+
 class ChannelPanel(tk.Frame):
-    def __init__(self, parent, dmx=None, channels=16):
+    def __init__(self, parent, dmx=None, channels=16, encoder_display=None):
         super().__init__(parent)
         self.dmx = dmx
+        self.encoder_display = encoder_display
         self.sliders = []
         self.selected_channel = 1
+        self.edit_mode = False
 
         for channel in range(1, channels + 1):
             frame = tk.Frame(self)
@@ -31,7 +34,28 @@ class ChannelPanel(tk.Frame):
         self.selected_channel = channel
         if self.dmx:
             self.dmx.set_channel(channel, int(float(value)))
+        self.update_display()
+
+    def next_channel(self):
+        if self.selected_channel < len(self.sliders):
+            self.selected_channel += 1
+        self.update_display()
+
+    def previous_channel(self):
+        if self.selected_channel > 1:
+            self.selected_channel -= 1
+        self.update_display()
+
+    def toggle_edit_mode(self):
+        self.edit_mode = not self.edit_mode
+        self.update_display()
+
+    def update_display(self):
+        if self.encoder_display:
+            value = self.sliders[self.selected_channel - 1][1].get()
+            self.encoder_display.update(self.selected_channel, value, self.edit_mode)
 
     def set_selected(self, channel):
         if 1 <= channel <= len(self.sliders):
             self.selected_channel = channel
+            self.update_display()
