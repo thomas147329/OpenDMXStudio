5a1365287f89b344ec08726005aa1cb5f9adc20a
thomas147329 <thomasjhebda@gmail.com>
2026-08-07 09:36:04 -0500

    Connect Arduino controller startup

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import tkinter as tk
+
 from gui import OpenDMXStudioGUI
+from controllers.arduino_controller import ArduinoController
 
 
 if __name__ == "__main__":
     root = tk.Tk()
+
     app = OpenDMXStudioGUI(root)
+
+    # Start Arduino controller
+    arduino = ArduinoController(app)
+    app.arduino = arduino
+
     root.mainloop()
